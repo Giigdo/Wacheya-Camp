bdfad2fc856caa1cd5f1c646d2508ab06fe2d88d
Giigdo <mcconnelltlss@gmail.com>
2026-03-13 20:42:18 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Wacheya Camp",
+    pageTitle: "Aandaakinogewin",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
